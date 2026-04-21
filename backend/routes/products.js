26176ea240d9e879f4b0536479e7214f761fa086
Jaipradeep K <kkrpradeep2007@gmail.com>
2026-04-21 20:42:39 +0530

    Update products.js

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Products API working ✅');
+  res.json([
+    { id: 1, name: "Black Cap", price: 299 },
+    { id: 2, name: "White Cap", price: 399 }
+  ]);
 });
 
 module.exports = router;
