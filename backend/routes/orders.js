6cd12c9720ac8f85c9c1d7d77e3cb76faca86d63
Jaipradeep K <kkrpradeep2007@gmail.com>
2026-04-21 20:09:22 +0530

    Update orders.js

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-// FIXED ROUTE
 router.get('/', (req, res) => {
-    res.send('Orders API working ✅');
+  res.send('Orders API working ✅');
 });
 
 module.exports = router;
