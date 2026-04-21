7f05f6a8c2c46520177dcc2280ca007d275f47de
Jaipradeep K <kkrpradeep2007@gmail.com>
2026-04-21 20:12:43 +0530

    Update payments.js

--- backend/routes/payments.js
+++ backend/routes/payments.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Payments API working ✅');
+  res.send('Payments API working ✅');
 });
 
 module.exports = router;
