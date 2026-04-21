3e295ba0d60c4f26c9a18cbb5d3d5f4bf251d490
Jaipradeep K <kkrpradeep2007@gmail.com>
2026-04-21 20:03:45 +0530

    Update auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
+// TEST
 router.get('/', (req, res) => {
   res.send('Auth API working ✅');
+});
+
+// REGISTER
+router.post('/register', (req, res) => {
+  res.send('User registered');
+});
+
+// LOGIN
+router.post('/login', (req, res) => {
+  res.send('User logged in');
 });
 
 module.exports = router;
